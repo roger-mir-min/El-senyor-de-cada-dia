bb4a235aae9e410c731f3ba617d825aa24645669
Roger Miret <r.mir.min@gmail.com>
2023-06-25 17:06:08 +0200

    Auto-generated commit [ci skip]

---
+++ assets/data/punts.ts
@@ -0,0 +1,22 @@
+import { Punt } from '../../shared/models/interfaces';
+
+export const datasetPuntsArray: Punt[]  = [
+    {
+        name: 'Viena',
+        lat: 41.54057767555261,
+        lng: 2.1124358919492825,
+        descripcio: "ambient tranquil, música baixa, poca gent, bon cafè.",
+        puntuacio: 7,
+        fav: false,
+        default: true
+    },
+    {
+        name: 'Canalla',
+        lat: 41.5470288743763,
+        lng: 2.108373638074375,
+        descripcio: "Menjar fantàstic, bona relació qualitat-preu",
+        puntuacio: 9,
+        fav: false,
+        default: true
+    }
+];
