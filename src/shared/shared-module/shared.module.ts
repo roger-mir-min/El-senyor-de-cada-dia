82cabe8416c75a99cbff14e20cd984cab08ab76c
Roger Miret <r.mir.min@gmail.com>
2023-06-27 10:18:24 +0200

    arrange header

--- src/shared/shared-module/shared.module.ts
+++ src/shared/shared-module/shared.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedComponent } from './shared.component';
+import { CardCollectionComponent } from '../components/card-collection/card-collection.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SharedComponent]
+  exports: [
+    CardCollectionComponent
+  ],
+  declarations: [
+    SharedComponent,
+    CardCollectionComponent
+  ]
 })
 export class SharedModule { }
